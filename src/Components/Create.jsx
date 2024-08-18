42692eb31eb2cd8959ea02fe326f7b4105fa5857
Dhruv Sharma <143330910+iDhruv11@users.noreply.github.com>
2024-08-18 21:04:46 +0530

    Update Create.jsx

--- src/Components/Create.jsx
+++ src/Components/Create.jsx
@@ -2,6 +2,7 @@ import { useRef, useState } from "react";
 import { validateEmail } from "../Utils/validateEmail";
 import { validateNumber } from "../Utils/validateNumber";
 import { setEmployee } from "../Utils/setEmployee";
+import { post } from "../Utils/post"
 
 const Create = () => {
     const name = useRef();
@@ -38,8 +39,7 @@ const Create = () => {
             return ;
         }
         const employee = setEmployee(name.current.value, mail.current.value, contactNum.current.value, country.current.value, deptName.current.value, empType.current.value);
-        post(employee);
-        
+        post(employee);       
           
     }
     return (
