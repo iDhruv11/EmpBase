b11f730acb664f118710afa717071b2dfcfdd381
Dhruv Sharma <143330910+iDhruv11@users.noreply.github.com>
2024-08-18 20:54:10 +0530

    Update post.js

--- src/Utils/post.js
+++ src/Utils/post.js
@@ -1,4 +1,16 @@
-const post = ( employee) => {
+import axios from "axios";
+
+const post = ( employee ) => {
+    const url = "localhost:5757/saved";
+    axios.post(url, employee)
+        .then((res)=>{
+            console.log(res);
+            
+        })
+        .catch((err)=>{
+            console.log(err);
+            
+        })
     
 }
 export { post };
